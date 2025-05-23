fd81ef2fdb9ca2883e46aefcc3efde2b50cb62c6
simjiv <simjiv@outlook.com>
2025-05-23 19:33:45 +0200

    Update farfetch_d.asm
    (+3 HP, +10 ATK, +10 DEF, +10 SPD, +10 SDF, Stick 23%)

--- data/pokemon/base_stats/farfetch_d.asm
+++ data/pokemon/base_stats/farfetch_d.asm
@@ -1,12 +1,12 @@
 	db FARFETCH_D ; 083
 
-	db  52,  65,  55,  60,  58,  62
+	db  55,  75,  65,  70,  58,  72
 	;   hp  atk  def  spd  sat  sdf
 
 	db NORMAL, FLYING ; type
 	db 45 ; catch rate
 	db 94 ; base exp
-	db NO_ITEM, STICK ; items
+	db STICK, STICK ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 5 ; step cycles to hatch
